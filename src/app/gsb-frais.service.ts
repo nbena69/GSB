be17ae7876e5b23ac2208dac249518b01fdc1fe4
nbena69 <126596584+nbena69@users.noreply.github.com>
2023-12-12 12:33:16 +0100

    En cours

--- src/app/gsb-frais.service.ts
+++ src/app/gsb-frais.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Frais } from './frais';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GsbFraisService {
+  private apiUrl = 'http://gsb.benaissa.etu.lmdsio.com/api/frais';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllFrais(): Observable<Frais[]> {
+    return this.http.get<Frais[]>(this.apiUrl);
+  }
+
+  deleteFrais(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
