79f31b9bdc281a2ca0e76b3507b5fb67ca9fec22
nbena69 <naelbenaissapro@gmail.com>
2023-12-17 19:20:15 +0100

    Page Home en cours

--- src/app/composant/home/home.component.ts
+++ src/app/composant/home/home.component.ts
@@ -1,15 +1,27 @@
 import {Component} from '@angular/core';
 import {MenuComponent} from "../menu/menu.component";
+import {ListeFraisComponent} from "../frais/liste-frais/liste-frais.component";
+import {GsbLoginService} from "../../service/gsb-login.service";
+import {CommonModule} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    MenuComponent
-  ],
+    imports: [
+        MenuComponent,
+        ListeFraisComponent, CommonModule, RouterLink
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  utilisateurConnecte: boolean = false;
+  constructor(private loginService: GsbLoginService) {
+  }
 
+  ngOnInit() {
+    this.utilisateurConnecte = this.loginService.estConnecte();
+    console.log(this.utilisateurConnecte);
+  }
 }
