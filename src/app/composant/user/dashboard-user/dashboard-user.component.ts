700045efe826a225f68d4335ff8a67e8daeb9a9c
nbena69 <naelbenaissapro@gmail.com>
2024-01-03 12:51:10 +0100

    Profil en cours

--- src/app/composant/user/dashboard-user/dashboard-user.component.ts
+++ src/app/composant/user/dashboard-user/dashboard-user.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {LeftSidebarComponent} from "./left-sidebar/left-sidebar.component";
+import {RightContentComponent} from "./right-content/right-content.component";
 
 @Component({
   selector: 'app-dashboard-user',
   standalone: true,
-  imports: [],
+  imports: [
+    LeftSidebarComponent,
+    RightContentComponent
+  ],
   templateUrl: './dashboard-user.component.html',
   styleUrl: './dashboard-user.component.css'
 })
 export class DashboardUserComponent {
+  selectedSection: string = '';
 
+  onSectionSelected(section: string) {
+    this.selectedSection = section;
+  }
 }
