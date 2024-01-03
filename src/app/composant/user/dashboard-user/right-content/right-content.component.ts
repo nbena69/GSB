013d516f042cfbc7ba3ab4af3c4a8f8614f230bf
nbena69 <naelbenaissapro@gmail.com>
2024-01-03 12:47:15 +0100

    Profil en cours

---
+++ src/app/composant/user/dashboard-user/right-content/right-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './right-content.component.html',
+  styleUrl: './right-content.component.css'
+})
+export class RightContentComponent {
+
+}
