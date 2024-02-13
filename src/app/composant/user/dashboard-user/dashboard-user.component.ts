9895434ab86a564f3468a32670f05b1a325c94b6
nbena69 <naelbenaissapro@gmail.com>
2024-02-13 11:27:00 +0100

    Reorganisation dashbord en cours

--- src/app/composant/user/dashboard-user/dashboard-user.component.ts
+++ src/app/composant/user/dashboard-user/dashboard-user.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
-import {LeftSidebarComponent} from "./left-sidebar/left-sidebar.component";
-import {RightContentComponent} from "./right-content/right-content.component";
+import {MenuUserComponent} from "../menu-user/menu-user.component";
 
 @Component({
   selector: 'app-dashboard-user',
   standalone: true,
   imports: [
-    LeftSidebarComponent,
-    RightContentComponent
+    MenuUserComponent
   ],
   templateUrl: './dashboard-user.component.html',
   styleUrl: './dashboard-user.component.css'
 })
 export class DashboardUserComponent {
-  selectedSection: string = '';
-
-  onSectionSelected(section: string) {
-    this.selectedSection = section;
-  }
 }
