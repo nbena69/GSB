a880f8ee356558509072283c8a214d5af423b1d2
nbena69 <naelbenaissapro@gmail.com>
2024-03-15 13:51:06 +0100

    Debut filtre visiteur

--- src/app/composant/user/search-visiteur/search-visiteur.component.ts
+++ src/app/composant/user/search-visiteur/search-visiteur.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {GsbShortService} from "../../../service/gsb-short.service";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-search-visiteur',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './search-visiteur.component.html',
   styleUrl: './search-visiteur.component.css'
 })
 export class SearchVisiteurComponent {
+  id_visiteur: FormControl = new FormControl("");
 
+  constructor(private shortService: GsbShortService) {
+    this.shortService.getListeVisiteur();
+  }
+
+  getListeVisiteur() {
+    return this.shortService.appels_terminesVisiteur;
+  }
 }
