1f869567da7d76b1c65111f51d27b69e1785b0f0
nbena69 <naelbenaissapro@gmail.com>
2024-03-24 13:50:24 +0100

    Modif en utilisant Material terminée

--- src/app/composant/frais/liste-frais/liste-frais.component.ts
+++ src/app/composant/frais/liste-frais/liste-frais.component.ts
@@ -3,12 +3,21 @@ import {MenuComponent} from "../../all/menu/menu.component";
 import {Router, RouterLink} from "@angular/router";
 import {GsbFraisService} from "../../../service/gsb-frais.service";
 import {CommonModule, Location} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+import {MatTableModule} from "@angular/material/table";
+import {MatDialogModule} from "@angular/material/dialog";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ErrorMessageComponent} from "../../all/error-message/error-message.component";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-liste-frais',
   standalone: true,
   imports: [
-    MenuComponent, RouterLink, CommonModule
+    MenuComponent, CommonModule, ReactiveFormsModule, MatDialogModule, RouterLink, ErrorMessageComponent, MatInputModule, MatIcon, MatTableModule, MatButtonModule, MatFormField, MatLabel, MatSelect, MatOption
   ],
   templateUrl: './liste-frais.component.html',
   styleUrl: './liste-frais.component.css',
