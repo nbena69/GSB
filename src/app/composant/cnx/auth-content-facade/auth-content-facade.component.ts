f8082d31034fbdf71a4065b9175140335c4c1a6a
nbena69 <naelbenaissapro@gmail.com>
2024-04-19 13:12:04 +0200

    Refractor Auth fait - Début css login

--- src/app/composant/cnx/auth-content-facade/auth-content-facade.component.ts
+++ src/app/composant/cnx/auth-content-facade/auth-content-facade.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-auth-content-facade',
   standalone: true,
-    imports: [
-        MatIcon
-    ],
+  imports: [
+    MatIcon,
+    RouterLink
+  ],
   templateUrl: './auth-content-facade.component.html',
   styleUrl: './auth-content-facade.component.css'
 })
