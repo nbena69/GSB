2a2460ade97439d1015b1f887e966508482ba5f7
nbena69 <naelbenaissapro@gmail.com>
2024-05-07 19:22:53 +0200

    Gestion de cookie en cours

---
+++ src/app/service/service-cookie/cookie.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CookieService {
+  constructor(private ngxCookieService: CookieService) { }
+
+  // Méthode pour stocker un cookie
+  setCookie(key: string, value: string, expires?: number | Date, path?: string, domain?: string, secure?: boolean, sameSite?: 'Lax' | 'Strict' | 'None') {
+    this.ngxCookieService.set(key, value, expires, path, domain, secure, sameSite);
+  }
+
+  // Méthode pour récupérer la valeur d'un cookie
+  getCookie(key: string): string {
+    return this.ngxCookieService.get(key);
+  }
+
+  // Méthode pour supprimer un cookie
+  deleteCookie(key: string, path?: string, domain?: string) {
+    this.ngxCookieService.delete(key, path, domain);
+  }
+}
