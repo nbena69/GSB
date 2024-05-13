3f566623a5f69b2267f8b216e4b61d639ac74c80
nbena69 <naelbenaissapro@gmail.com>
2024-05-13 13:55:02 +0200

    Utilistation api gouv pour adresse

--- src/app/composant/classic/activite/ajout-activite/ajout-activite.component.ts
+++ src/app/composant/classic/activite/ajout-activite/ajout-activite.component.ts
@@ -8,11 +8,15 @@ import {MatFormField, MatFormFieldModule, MatHint, MatLabel} from "@angular/mate
 import {MatInput, MatInputModule} from "@angular/material/input";
 import {ErrorMessageComponent} from "../../../all/error-message/error-message.component";
 import {GsbAllService} from "../../../../service/gsb-all.service";
+import {MatList, MatListItem} from "@angular/material/list";
+import {Subscription} from "rxjs";
+import {Adresse} from "../../../../metier/api-ban/adresse";
+import {BanService} from "../../../../service/service-ban/ban.service";
 
 @Component({
   selector: 'app-ajout-activite',
   standalone: true,
-  imports: [MenuComponent, ReactiveFormsModule, CommonModule, MatFormField, MatLabel, MatHint, MatInput, MatFormFieldModule, MatInputModule, ErrorMessageComponent],
+  imports: [MenuComponent, ReactiveFormsModule, CommonModule, MatFormField, MatLabel, MatHint, MatInput, MatFormFieldModule, MatInputModule, ErrorMessageComponent, MatList, MatListItem],
   templateUrl: './ajout-activite.component.html',
   styleUrl: './ajout-activite.component.css'
 })
@@ -21,9 +25,32 @@ export class AjoutActiviteComponent {
   lieu_activite: FormControl = new FormControl('');
   theme_activite: FormControl = new FormControl('');
   motif_activite: FormControl = new FormControl('');
+  addressSubscription: Subscription;
+  adresses: Adresse[] = [];
   errorMessage: string | null = null;
 
-  constructor(private all_service: GsbAllService, route: ActivatedRoute, private activite_api: GsbActiviteService) {
+  constructor(private all_service: GsbAllService, route: ActivatedRoute, private activite_api: GsbActiviteService, private banService: BanService) {
+    this.addressSubscription = this.lieu_activite.valueChanges.subscribe(value => {
+      this.banService.searchAddress(value).subscribe(addresses => {
+        // Traiter les adresses retournées
+        // Par exemple, vous pouvez mettre à jour une liste d'autocomplétion avec les adresses retournées
+      });
+    });}
+
+  searchAddress() {
+    const query = this.lieu_activite.value;
+    if (query) {
+      this.banService.searchAddress(query).subscribe((addresses: Adresse[]) => {
+        this.adresses = addresses;
+      });
+    } else {
+      this.adresses = [];
+    }
+  }
+
+  selectAddress(address: Adresse) {
+    this.lieu_activite.setValue(address.label);
+    this.adresses = [];
   }
 
   onSubmitAjoutActivite() {
