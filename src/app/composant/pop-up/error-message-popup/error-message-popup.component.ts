b266858ff8bc0044b8a0ea09140a3073f4bc6bf7
nbena69 <naelbenaissapro@gmail.com>
2024-05-15 17:47:22 +0200

    Popup error message en cours

--- src/app/composant/pop-up/error-message-popup/error-message-popup.component.ts
+++ src/app/composant/pop-up/error-message-popup/error-message-popup.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-error-message-popup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './error-message-popup.component.html',
   styleUrl: './error-message-popup.component.css'
 })
 export class ErrorMessagePopupComponent {
-
+  errorMessage: string | null = null;
+  closeErrorMessage() {
+    this.errorMessage = null;
+  }
 }
