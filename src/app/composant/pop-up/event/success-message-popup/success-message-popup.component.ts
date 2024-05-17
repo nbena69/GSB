1c66cd60be9baf7d3e6f3a872181397ee71267be
nbena69 <naelbenaissapro@gmail.com>
2024-05-17 19:01:58 +0200

    Ajout success popup

---
+++ src/app/composant/pop-up/event/success-message-popup/success-message-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-message-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './success-message-popup.component.html',
+  styleUrl: './success-message-popup.component.css'
+})
+export class SuccessMessagePopupComponent {
+
+}
